c65cd1804adb2a319eec8dc6b860b13f6e846da6
Jon Freivald <jfreivald@brmedical.com>
2025-12-22 16:01:33 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,16 +2,14 @@
 echo Sourcing from: %~dp0
 C:
 cd %USERPROFILE%
-mkdir BatMenu
+if not exist BatMenu mkdir BatMenu
 cd BatMenu
-echo Installing Python 3.11.5
-"%~dp0\python-3.11.5-amd64.exe" /passive
 echo creating virtual environment
-%LocalAppData%\Programs\Python\Python311\python.exe -m venv venv
+python -m venv venv
 echo activating virtual environment
 call venv\scripts\activate
 echo upgrading pip
-%LocalAppData%\Programs\Python\Python311\python.exe -m pip install --upgrade pip
+venv\scripts\python.exe -m pip install --upgrade pip
 echo Adding package requirements
-%LocalAppData%\Programs\Python\Python311\Scripts\pip.exe install -r "%~dp0\requirements.txt"
+venv\scripts\pip.exe install -r "%~dp0\requirements.txt"
 deactivate
